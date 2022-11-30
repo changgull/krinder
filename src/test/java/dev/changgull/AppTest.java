e6b88204f581f231ab374c34c047c8367ffda2b1
Changgull Song <changgull@gmail.com>
2022-11-30 10:27:27 -0800

    Initial commit

---
+++ src/test/java/dev/changgull/AppTest.java
@@ -0,0 +1,19 @@
+package dev.changgull;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+}
